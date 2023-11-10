00935f94841d53f5667b5b960594c61f9c9678ff
lucasdepetrisd <lucasdepetris14@gmail.com>
2023-11-10 16:37:14 -0300

    Agrego ServicioApi.cs y soluciono error CORS

--- ProyectoPAD-Simpson/Program.cs
+++ ProyectoPAD-Simpson/Program.cs
@@ -1,27 +1,12 @@
 using Microsoft.AspNetCore.Components.Web;
 using Microsoft.AspNetCore.Components.WebAssembly.Hosting;
 using ProyectoPAD_Simpson;
-using Microsoft.AspNetCore.Builder;
-
 
 var builder = WebAssemblyHostBuilder.CreateDefault(args);
 builder.RootComponents.Add<App>("#app");
 builder.RootComponents.Add<HeadOutlet>("head::after");
 
+builder.Services.AddScoped<ServicioApi>();
 builder.Services.AddScoped(sp => new HttpClient { BaseAddress = new Uri(builder.HostEnvironment.BaseAddress) });
-
-
-var MyAllowSpecificOrigins = "_myAllowSpecificOrigins";
-
-
-builder.Services.AddCors(options =>
-{
-    options.AddPolicy(name: MyAllowSpecificOrigins,
-                      policy =>
-                      {
-                          policy.WithOrigins("https://frinkiac.com");
-                      });
-});
-
 
 await builder.Build().RunAsync();
---
+++ ProyectoPAD-Simpson/ServicioApi.cs
@@ -0,0 +1,23 @@
+﻿using System.Collections.Generic;
+using System.Net.Http;
+using System.Net.Http.Json;
+using System.Threading.Tasks;
+using static ProyectoPAD_Simpson.Pages.Simpson;
+
+public class ServicioApi
+{
+    private readonly HttpClient _httpClient;
+
+    public ServicioApi(HttpClient httpClient)
+    {
+        _httpClient = httpClient;
+    }
+
+    public async Task<List<Episode>> ObtenerEpisodios(string query)
+    {
+        var resultado = await _httpClient.GetFromJsonAsync<List<Episode>>(query);
+
+        // Verificar si resultado es nulo y devolver una lista vacía en su lugar
+        return resultado ?? new List<Episode>();
+    }
+}
