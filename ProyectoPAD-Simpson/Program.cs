15821866b23cbfa594ffb1db9942eb4de8ab9534
Lucas Depetris <lucasdepetris14@gmail.com>
2023-11-11 00:48:37 -0300

    Adiciones y refactorizaciones. Leer descripción.
    Agrego busqueda aleatoria, información de frases e imagenes de episodio
    Agrego método ObtenerEpisodio en ServicioApi.cs
    Refactorizo clases y variables de Simpson.razor y renombro ServicioApi.cs a ServicioApiEpisodios.cs
    Modifico visuales y texto en Simpson.razor

--- ProyectoPAD-Simpson/Program.cs
+++ ProyectoPAD-Simpson/Program.cs
@@ -6,7 +6,7 @@ var builder = WebAssemblyHostBuilder.CreateDefault(args);
 builder.RootComponents.Add<App>("#app");
 builder.RootComponents.Add<HeadOutlet>("head::after");
 
-builder.Services.AddScoped<ServicioApi>();
+builder.Services.AddScoped<ServicioApiEpisodios>();
 builder.Services.AddScoped(sp => new HttpClient { BaseAddress = new Uri(builder.HostEnvironment.BaseAddress) });
 
 await builder.Build().RunAsync();
