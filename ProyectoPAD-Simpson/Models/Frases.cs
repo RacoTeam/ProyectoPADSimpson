607e1aff73f567203a12251329899fbabeeb203a
emanuelharo <emanuelharo72@gmail.com>
2023-11-12 14:58:27 -0300

    Empiezo con dbContext

---
+++ ProyectoPAD-Simpson/Models/Frases.cs
@@ -0,0 +1,13 @@
+﻿using System.ComponentModel.DataAnnotations;
+
+namespace ProyectoPADSimpson.Models
+{
+    public class Frases
+    {
+        [Key]
+        public int IdFrases { get; set; }
+        public string Usuario { get; set; }
+        public string Frase { get; set; }
+        public string Capitulo { get; set; }
+    }
+}
---
+++ ProyectoPAD-Simpson/Models/Usuario.cs
@@ -0,0 +1,12 @@
+﻿using System.ComponentModel.DataAnnotations;
+
+namespace ProyectoPADSimpson.Models
+{
+    public class Usuario
+    {
+        [Key]
+        public int IdUsuario { get; set; }
+        public string Nombre { get; set; }
+        public string Clave { get; set; }
+    }
+}
