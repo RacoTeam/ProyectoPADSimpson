430a080aa1c7994eaf95b17294e146f833aad876
Lucas Depetris <lucasdepetris14@gmail.com>
2023-11-12 20:38:42 -0300

    Super mega migracion

---
+++ Shared/Models/Frase.cs
@@ -0,0 +1,16 @@
+﻿using System.ComponentModel.DataAnnotations;
+
+namespace ProyectoPADSimpson.Shared.Models
+{
+    public class Frase
+    {
+        [Key]
+        public int Id { get; set; }
+        [Required]
+        public string Texto { get; set; }
+        [Required]
+        public string Capitulo { get; set; }
+        [Required]
+        public int IdUsuario { get; set; }
+    }
+}
