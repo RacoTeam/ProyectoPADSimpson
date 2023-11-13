b93c15ae7bf413fd3a8252fbee0a45fca75a04fe
emanuelharo <emanuelharo72@gmail.com>
2023-11-12 23:16:50 -0300

    SQL Conection Azure

---
+++ ProyectoPADSimpson.Models/Frase.cs
@@ -0,0 +1,17 @@
+﻿using System;
+using System.Collections.Generic;
+
+namespace ProyectoPADSimpson.Models;
+
+public partial class Frase
+{
+    public int IdFrases { get; set; }
+
+    public int? IdUsuario { get; set; }
+
+    public string? Frase1 { get; set; }
+
+    public string? Capitulo { get; set; }
+
+    public virtual Usuario? IdUsuarioNavigation { get; set; }
+}
