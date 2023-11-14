4561b98d9a3190fee4f52776cc63b042aa730827
emanuelharo <emanuelharo72@gmail.com>
2023-11-14 17:18:36 -0300

    Front de login

--- Shared/Models/Usuario.cs
+++ Shared/Models/Usuario.cs
@@ -10,5 +10,7 @@ namespace ProyectoPADSimpson.Shared.Models
         public string Username { get; set; }
         [Required]
         public string Password { get; set; }
+        [Required]
+        public string ConfirmPassword { get; set; }
     }
 }
