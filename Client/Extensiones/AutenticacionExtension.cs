b4e25c88d6a9e1650ae4e300e0c0053b826bffa8
emanuelharo <emanuelharo72@gmail.com>
2023-11-17 16:05:36 -0300

    Comienzo autorizacion

---
+++ Client/Extensiones/AutenticacionExtension.cs
@@ -0,0 +1,23 @@
+﻿using Blazored.LocalStorage;
+using ProyectoPADSimpson.Shared;
+using Microsoft.AspNetCore.Components.Authorization;
+using System.Security.Claims;
+
+namespace ProyectoPADSimpson.Client.Extensiones
+{
+    public class AutenticacionExtension: AuthenticationStateProvider
+    {
+        private readonly ILocalStorageService _localStorage;
+        //private ClaimsPrincipal _sinInformacion = new ClaimsPrincipal(new ClaimsIdentity());
+
+        //public AutenticacionExtension(ILocalStorageService localStorage)
+        //{
+        //    _localStorage = localStorage;
+        //}
+
+        public override Task<AuthenticationState> GetAuthenticationStateAsync()
+        {
+            throw new NotImplementedException();
+        }
+    }
+}
