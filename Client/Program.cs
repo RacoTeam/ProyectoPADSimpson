1a11ca3ffc2519fd522a686606ecb1d12fc2b583
lucasdepetrisd <lucasdepetris14@gmail.com>
2023-11-16 22:32:27 -0300

    Refactorizo y soluciono problema puertos

--- Client/Program.cs
+++ Client/Program.cs
@@ -3,7 +3,7 @@ using Microsoft.AspNetCore.Components.Web;
 using Microsoft.AspNetCore.Components.WebAssembly.Hosting;
 using Blazored.LocalStorage;
 using Blazored.Toast;
-using ProyectoPADSimpson.Client.Servicios;
+using ProyectoPADSimpson.Client.Services;
 
 var builder = WebAssemblyHostBuilder.CreateDefault(args);
 builder.RootComponents.Add<App>("#app");
@@ -12,8 +12,9 @@ builder.RootComponents.Add<HeadOutlet>("head::after");
 builder.Services.AddBlazoredLocalStorage();
 builder.Services.AddBlazoredToast();
 
-builder.Services.AddScoped(sp => new HttpClient { BaseAddress = new Uri("https://localhost:7098") });
+builder.Services.AddScoped(sp => new HttpClient { BaseAddress = new Uri(builder.HostEnvironment.BaseAddress) });
 
 builder.Services.AddScoped<IUsuarioServicio, UsuarioServicio>();
+builder.Services.AddScoped<IFraseServicio, FraseServicio>();
 
 await builder.Build().RunAsync();
